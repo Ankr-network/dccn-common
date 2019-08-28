8213ba83d0a68886848bd1046c495abd35153c3f
jensen_Lv <lyl_office@163.com>
2019-08-28 15:24:38 +0800

    Swdev 405 crypt (#176)
    * added decryptDataV3 and encryptDataV3
    
    * testing add index.js
    
    * updated crypt sample
    
    * add readme file
    
    * fix some issues
    
    * updated readme.md in English
    
    * updated encryptData and decryptData name
    
    * updated readme file

--- blockchain/ethereum/service.go
+++ blockchain/ethereum/service.go
@@ -1,63 +1,63 @@
 package ethereum
 
 import (
-	"fmt"
-	"math"
-	"math/big"
-	"strings"
+    "fmt"
+    "math"
+    "math/big"
+    "strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/accounts/abi/bind"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
 )
 
 const (
-	EthereumNodeURL = "https://kindly-civil-escargot.quiknode.io/baa643a7-78b4-438e-b53a-76569410a448/bEpPhdsXJUrNvWzednALKg==/"
+    EthereumNodeURL = "https://kindly-civil-escargot.quiknode.io/baa643a7-78b4-438e-b53a-76569410a448/bEpPhdsXJUrNvWzednALKg==/"
 )
 
 type EthService struct {
-	EthClient *ethclient.Client
+    EthClient *ethclient.Client
 }
 
-func NewEthService() (*EthService, error){
-	c, err := ethclient.Dial(EthereumNodeURL)
-	if err != nil {
-		return  nil, err
-	}
+func NewEthService() (*EthService, error) {
+    c, err := ethclient.Dial(EthereumNodeURL)
+    if err != nil {
+        return nil, err
+    }
 
-	return &EthService{c }, nil
+    return &EthService{c}, nil
 }
 
-func (s *EthService) TokenTransfer(assertName, fromKey, fromPassword, toAddress string, amount *big.Float) error {
-	token, err := newToken(assertName, s.EthClient)
-	if err != nil {
-		return err
-	}
+func (s *EthService) TokenTransfer(assertName, fromKey, fromPassword, toAddress string, amount *big.Float) (hash string, err error) {
+    token, err := newToken(assertName, s.EthClient)
+    if err != nil {
+        return hash, err
+    }
 
-	toAddr := common.HexToAddress(toAddress)
+    toAddr := common.HexToAddress(toAddress)
 
-	auth, err := bind.NewTransactor(strings.NewReader(fromKey), fromPassword)
-	if err != nil {
+    auth, err := bind.NewTransactor(strings.NewReader(fromKey), fromPassword)
+    if err != nil {
 
-		return err
-	}
+        return hash, err
+    }
 
-	decimal, err := token.Decimals(nil)
-	if err != nil {
-		return err
-	}
+    decimal, err := token.Decimals(nil)
+    if err != nil {
+        return hash, err
+    }
 
-	tenDecimal := big.NewFloat(math.Pow(10, float64(decimal)))
-	tens := tenDecimal.String()
+    tenDecimal := big.NewFloat(math.Pow(10, float64(decimal)))
+    tens := tenDecimal.String()
 
-	fmt.Printf("%s, %s\n", tens, amount.String())
-	convertAmountF := new(big.Float).Mul(tenDecimal, amount)
-	convertAmount  := new(big.Int)
-	convertAmountF.Int(convertAmount)
+    fmt.Printf("%s, %s\n", tens, amount.String())
+    convertAmountF := new(big.Float).Mul(tenDecimal, amount)
+    convertAmount := new(big.Int)
+    convertAmountF.Int(convertAmount)
 
-	fmt.Printf("%s", convertAmount.String())
+    fmt.Printf("%s", convertAmount.String())
 
-	_, err = token.transfer(auth, toAddr, convertAmount)
+    tx, err := token.transfer(auth, toAddr, convertAmount)
 
-	return err
+    return tx.Hash().Hex(), err
 }
