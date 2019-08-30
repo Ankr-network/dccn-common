a5af24597b6ddfa7d6b0e87daa9f6eaf50fc681c
mobius <sunvim@users.noreply.github.com>
2019-08-30 16:03:10 +0800

    Feature/swdev 420 ankrmicro (#180)
    * supoort mongodb compatible with old way which no secrets
    
    * solve conflict
    
    * solve conflict again
    
    * add db options

--- ankr-micro/mongodb.go
+++ ankr-micro/mongodb.go
@@ -23,26 +23,27 @@ func GetCollection(collection string) *mgo.Collection {
 // GetDBInstance return an instance of mongo db instance
 func GetDBInstance() *mgo.Database {
 	once.Do(func() {
-		instance = mongodbconnect()
+		instance = mongodbConnect()
 	})
 	return instance
 }
 
-func mongodbconnect() *mgo.Database {
-	config := GetConfig()
-	logStr := fmt.Sprintf("mongodb hostname : %s", config.DatabaseHost)
-	WriteLog(logStr)
-	session, err := mgo.Dial(config.DatabaseHost)
+func mongodbConnect() *mgo.Database {
+	session, err = CreateDBSession()
 	if err != nil {
-		WriteLog("can not connect to database")
-		return nil
+		panic(err)
 	}
-	//defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+	config := GetConfig()
+	if config.DbAuth {
+		mc, err := cfg.GetMgoConfig(config.VaultAddr, config.VaultRole, config.DataPath)
+		if err != nil {
+			panic(err)
+		}
+		return session.DB(mc.DbName)
+	}
 
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-	db := session.DB(config.DatabaseName)
-	return db
+	return session.DB(config.DatabaseName)
 }
 
 
@@ -59,12 +60,38 @@ func GetDB(database string)*mgo.Database{
 
 func CreateDBSession() (*mgo.Session,  error) {
 	config := GetConfig()
+
+	if config.DbAuth {
+		// support auth
+		mc, err := cfg.GetMgoConfig(config.VaultAddr, config.VaultRole, config.DataPath)
+		if err != nil {
+			panic("get secret from kms failed: " + err.Error())
+		}
+		logStr := fmt.Sprintf("mongodb hostname : %s", mc.Address)
+		WriteLog(logStr)
+		session, err := mgo.DialWithTimeout(
+			fmt.Sprintf("mongodb://%s:%s@%s", mc.UserName, mc.PassWd, mc.Address),
+			15*time.Second)
+		if err != nil {
+			panic(err)
+		}
+		if session != nil {
+			session.SetPoolLimit(int(mc.PoolSize))
+			session.SetMode(mgo.Monotonic, true)
+		} else {
+			panic("session can not be nil, please check")
+		}
+		return session, err
+	}
+
+	// compatible old way
 	logStr := fmt.Sprintf("mongodb hostname : %s", config.DatabaseHost)
 	WriteLog(logStr)
 	session, err := mgo.Dial(config.DatabaseHost)
 	if err != nil {
-		WriteLog("can not connect to database")
-		return session, err
+		panic(err)
 	}
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
 	return session, err
 }
