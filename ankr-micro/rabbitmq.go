dbc41deea886c91b3b817e901d7b78154fc7bfc7
leoliang <leoliang_sh@163.com>
2019-09-11 12:44:51 +0800

    [SWDEV-434] change ankr-micro exchange to durable

--- ankr-micro/rabbitmq.go
+++ ankr-micro/rabbitmq.go
@@ -53,7 +53,7 @@ func Send(topic string, e interface{}) error {
 	err = ch.ExchangeDeclare(
 		defaultExchange, // name
 		"topic",         // type
-		false,           // durable
+		true,           // durable
 		false,           // auto-deleted
 		false,           // internal
 		false,           // no-wait
@@ -121,7 +121,7 @@ func Receive(topic string, handler interface{}) {
 		err = ch.ExchangeDeclare(
 			defaultExchange, // name
 			"topic",         // type
-			false,           // durable
+			true,           // durable
 			false,           // auto-deleted
 			false,           // internal
 			false,           // no-wait
