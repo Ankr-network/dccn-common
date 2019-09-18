876f6512c68f5d847cc2fd0ce1bcc40f8bc8a241
jensen_Lv <lyl_office@163.com>
2019-09-18 11:12:53 +0800

    updated ankr and ankr test (#200)

--- blockchain/ethereum/token.go
+++ blockchain/ethereum/token.go
@@ -17,7 +17,12 @@ type Token interface {
 func newToken(name string, ethClient *ethclient.Client) (Token, error) {
     switch name {
     case "ANKR":
-        token, err := NewAnkrToken(name, ethClient)
+        contractAddress:="0x8290333cef9e6d528dd5618fb97a76f268f3edd4"
+        token, err := NewAnkrToken(name, ethClient,contractAddress)
+        return token, err
+    case "ANKR-T":
+        contractAddress:="0x38A03b35a6662D35B226879E57d499eE26A6D4B5"
+        token, err := NewAnkrToken(name, ethClient,contractAddress)
         return token, err
     case "USDT":
         token, err := NewUsdtToken(name, ethClient)
