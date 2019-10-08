182e394a03d03eb634784fba636cce7e44879995
Tony Wu <xiaowu@berkeley.edu>
2019-10-08 16:08:14 -0700

    [SWDEV-443]Update Extra two api for investmgr

--- protos/gateway/investmgr/v1/investmgr.pb.gw.go
+++ protos/gateway/investmgr/v1/investmgr.pb.gw.go
@@ -232,6 +232,40 @@ func request_InvestMgr_Withdraw_0(ctx context.Context, marshaler runtime.Marshal
 
 }
 
+func request_InvestMgr_UpdateRewardCurrency_0(ctx context.Context, marshaler runtime.Marshaler, client InvestMgrClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpdateRewardCurrencyRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.UpdateRewardCurrency(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+var (
+	filter_InvestMgr_GetRewardCurrency_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_InvestMgr_GetRewardCurrency_0(ctx context.Context, marshaler runtime.Marshaler, client InvestMgrClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq RewardCurrencyRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_InvestMgr_GetRewardCurrency_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetRewardCurrency(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterInvestMgrHandlerFromEndpoint is same as RegisterInvestMgrHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterInvestMgrHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -510,6 +544,46 @@ func RegisterInvestMgrHandlerClient(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
+	mux.Handle("POST", pattern_InvestMgr_UpdateRewardCurrency_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_InvestMgr_UpdateRewardCurrency_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_InvestMgr_UpdateRewardCurrency_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_InvestMgr_GetRewardCurrency_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_InvestMgr_GetRewardCurrency_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_InvestMgr_GetRewardCurrency_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -537,6 +611,10 @@ var (
 	pattern_InvestMgr_GetAccumulatedProfit_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"profit", "accumulated"}, ""))
 
 	pattern_InvestMgr_Withdraw_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"account", "withdraw"}, ""))
+
+	pattern_InvestMgr_UpdateRewardCurrency_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"reward", "update"}, ""))
+
+	pattern_InvestMgr_GetRewardCurrency_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"reward", "currency"}, ""))
 )
 
 var (
@@ -563,4 +641,8 @@ var (
 	forward_InvestMgr_GetAccumulatedProfit_0 = runtime.ForwardResponseMessage
 
 	forward_InvestMgr_Withdraw_0 = runtime.ForwardResponseMessage
+
+	forward_InvestMgr_UpdateRewardCurrency_0 = runtime.ForwardResponseMessage
+
+	forward_InvestMgr_GetRewardCurrency_0 = runtime.ForwardResponseMessage
 )
