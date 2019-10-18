5af72d9de9b31b52cb0d56a3c9971749487847f2
yanlonglv <lyl_office@163.com>
2019-10-19 06:26:13 +0800

    bitcoin send example

--- blockchain/example/btc_send_example.go
+++ blockchain/example/btc_send_example.go
@@ -1,39 +1,41 @@
 package main
 
 import (
-    "github.com/huahuayu/go-bitcoind"
-    "log"
+    "fmt"
+    "encoding/json"
+    "github.com/Ankr-network/dccn-common/blockchain/btc"
 )
 
-//func main() {
-//    transaction, err := btc.CreateTransaction("","cQpb8dJ3B36c25HGBSSDkzu4d8CRzP1tbWJg2vsWyAiEfetQPtdX", "2MytUZedXFVTCKXrgRc2cJJf8NTaurMegdA", 1, "959ee3e0971d12c9449d51d29b26cdd198821b899a6cb37ce3b51a2294bd6c8e")
-//    if err != nil {
-//        fmt.Println(err)
-//        return
-//    }
-//    data, _ := json.Marshal(transaction)
-//    fmt.Println(string(data))
-//}
+func main() {
+   transaction, err := btc.CreateTransaction("cQpb8dJ3B36c25HGBSSDkzu4d8CRzP1tbWJg2vsWyAiEfetQPtdX", "2MytUZedXFVTCKXrgRc2cJJf8NTaurMegdA", 1, "959ee3e0971d12c9449d51d29b26cdd198821b899a6cb37ce3b51a2294bd6c8e")
+   if err != nil {
+       fmt.Println(err)
+       return
+   }
+   data, _ := json.Marshal(transaction)
+   fmt.Println(string(data))
+}
 //wisdom simple consider flip faculty live shove liar dune remove dial toe
 
-const (
-    SERVER_HOST        = "47.75.70.201"
-    SERVER_PORT        = 18332
-    USER               = ""
-    PASSWD             = ""
-    USESSL             = false
-    WALLET_PASSPHRASE  = ""
-    WALLET_PASSPHRASE2 = ""
-)
-
-func main(){
-    bc, err := bitcoind.New(SERVER_HOST, SERVER_PORT, USER, PASSWD, USESSL)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    tx,err:=bc.SendFrom("","",0.00001,1,"","")
-    if err!=nil{
-        return tx ,err
-    }
-
-}
+//const (
+//    SERVER_HOST        = "47.75.70.201"
+//    SERVER_PORT        = 18332
+//    USER               = ""
+//    PASSWD             = ""
+//    USESSL             = false
+//    WALLET_PASSPHRASE  = ""
+//    WALLET_PASSPHRASE2 = ""
+//)
+//
+//func main(){
+//    bc, err := bitcoind.New(SERVER_HOST, SERVER_PORT, USER, PASSWD, USESSL)
+//    if err != nil {
+//        log.Fatalln(err)
+//    }
+//    tx,err:=bc.SendFrom("","",0.00001,1,"","")
+//    if err!=nil{
+//         log.Printf("%s",err)
+//    }
+//    fmt.Print(tx)
+//
+//}
