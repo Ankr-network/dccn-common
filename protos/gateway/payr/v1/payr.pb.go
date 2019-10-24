26f4593bd871f1152069d5a49f421c3eb3444f8d
ximingding <ximing@ankr.com>
2019-10-24 14:07:18 +0800

    [SWDEV-470] add new interface into logmgr

--- protos/gateway/payr/v1/payr.pb.go
+++ protos/gateway/payr/v1/payr.pb.go
@@ -10,8 +10,6 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -1106,32 +1104,6 @@ type PayrServer interface {
 	CancelOrder(context.Context, *TeamID) (*common.Empty, error)
 	ListPlan(context.Context, *common.Empty) (*ListPlanResponse, error)
 	ListSubs(context.Context, *ListSubsRequest) (*ListSubsResponse, error)
-}
-
-// UnimplementedPayrServer can be embedded to have forward compatible implementations.
-type UnimplementedPayrServer struct {
-}
-
-func (*UnimplementedPayrServer) CollectFee(ctx context.Context, req *CollectFeeRequest) (*CollectFeeResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CollectFee not implemented")
-}
-func (*UnimplementedPayrServer) PlanFee(ctx context.Context, req *PlanFeeRequest) (*CollectFeeResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method PlanFee not implemented")
-}
-func (*UnimplementedPayrServer) NewOrder(ctx context.Context, req *NewOrderRequest) (*NewOrderResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method NewOrder not implemented")
-}
-func (*UnimplementedPayrServer) OrderStatus(ctx context.Context, req *TeamID) (*OrderStatusResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method OrderStatus not implemented")
-}
-func (*UnimplementedPayrServer) CancelOrder(ctx context.Context, req *TeamID) (*common.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CancelOrder not implemented")
-}
-func (*UnimplementedPayrServer) ListPlan(ctx context.Context, req *common.Empty) (*ListPlanResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ListPlan not implemented")
-}
-func (*UnimplementedPayrServer) ListSubs(ctx context.Context, req *ListSubsRequest) (*ListSubsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ListSubs not implemented")
 }
 
 func RegisterPayrServer(s *grpc.Server, srv PayrServer) {
