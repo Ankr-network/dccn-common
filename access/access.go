97f15849e12c69ae5bbd1a6e5c06409b8daf9dde
leoliang <leoliang_sh@163.com>
2019-11-04 22:54:49 +0800

    [SWDEV-468] common access control api

---
+++ access/access.go
@@ -0,0 +1,46 @@
+package access
+
+import (
+	"context"
+	"fmt"
+	"github.com/Ankr-network/dccn-teammgr/api/protos/v1alpha/role"
+	"google.golang.org/grpc"
+)
+
+const (
+	defaultTeamMgrEndpoint = "dccn-teammgr:50051"
+)
+
+var (
+	defaultAuthorizationSvc = NewAuthorizationService(defaultTeamMgrEndpoint)
+)
+
+type AuthorizationService interface {
+	Authorize(ctx context.Context, sub, resource, action string) (bool, error)
+}
+
+func NewAuthorizationService(teamMgrEndpoint string) AuthorizationService {
+	return &teamMgrAuthorizationSvc{teamMgrEndpoint: teamMgrEndpoint}
+}
+
+type teamMgrAuthorizationSvc struct {
+	teamMgrEndpoint string
+}
+
+func (p *teamMgrAuthorizationSvc) Authorize(ctx context.Context, sub, resource, action string) (bool, error) {
+	conn, err := grpc.Dial(p.teamMgrEndpoint, grpc.WithInsecure())
+	if err != nil {
+		return false, fmt.Errorf("grpc dial %s error: %w", p.teamMgrEndpoint, err)
+	}
+	defer conn.Close()
+	client := role.NewInternalRoleClient(conn)
+	rsp, err := client.Authorize(ctx, &role.AuthorizeRequest{Uid: sub, Resource: resource, Action: action})
+	if err != nil {
+		return false, fmt.Errorf("grpc role.Authorize error: %w", err)
+	}
+	return rsp.Ok, nil
+}
+
+func Authorize(ctx context.Context, sub, resource, action string) (bool, error) {
+	return defaultAuthorizationSvc.Authorize(ctx, sub, resource, action)
+}
---
+++ access/go.mod
@@ -0,0 +1,10 @@
+module github.com/Ankr-network/dccn-common/access
+
+go 1.13
+
+require (
+	github.com/Ankr-network/dccn-common v0.0.0-20191104185450-047a2bf5021a
+	github.com/Ankr-network/dccn-teammgr v0.0.0-20191105032842-1170f03ce3b7
+	github.com/golang/protobuf v1.3.2
+	google.golang.org/grpc v1.24.0
+)
