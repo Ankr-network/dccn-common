42e8b9401ff9738662cd569157d82e83d01dc3d9
alvin <liukui1028@gmail.com>
2020-09-27 16:53:25 +0800

    node code

--- rabbitmqmsg/apinode/node_type.go
+++ rabbitmqmsg/apinode/node_type.go
@@ -1,6 +1,12 @@
 package apiNode
 
 const (
-	NodeType_EthMainNet     = "0011"
-	NodeType_BinanceMainNet = "0021"
+	NodeType_Eth_Fast_MainNet     = "0011"
+	NodeType_Eth_Fast_RopstenNet  = "0012"
+	NodeType_Eth_Fast_RinkebyNet  = "0013"
+	NodeType_Eth_Fast_GorliNet    = "0014"
+	NodeType_Binance_Full_MainNet = "0021"
+	NodeType_Binance_Fast_MainNet = "0022"
+	NodeType_Binance_Full_TestNet = "0023"
+	NodeType_Binance_Fast_TestNet = "0024"
 )
