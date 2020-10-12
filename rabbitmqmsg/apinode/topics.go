ba2b455bd49cf84dc0dec0a30aa574a32406dab5
alvin <liukui1028@gmail.com>
2020-10-12 10:53:44 +0800

    authorization

--- rabbitmqmsg/apinode/topics.go
+++ rabbitmqmsg/apinode/topics.go
@@ -3,4 +3,3 @@ package apiNode
 //go:generate protoc -I. --go_out=plugins=grpc:. api_node
 
 const ApiGatewayNodeRequestTopic = "ankr.api.api-gateway-node-request"
-const ApiGatewayNodeResponseTopic = "ankr.api.api-gateway-node-response"
